398aafab1cc39a4996215f09cd84c4eb096ec40a
FPT SHOP <thieuhungyn@gmail.com>
2025-05-13 10:37:04 +0700

    đây

--- app/src/main/java/com/example/apptruyen/Home/FirstFragment.java
+++ app/src/main/java/com/example/apptruyen/Home/FirstFragment.java
@@ -1,4 +1,5 @@
 package com.example.apptruyen.Home;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -6,15 +7,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.apptruyen.ComicDataLoader;
 import com.example.apptruyen.R;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.example.apptruyen.firebase.ComicAdapter;
+import com.example.apptruyen.model.Manga;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ComicAdapter adapter;
+    private List<Manga> comicList;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        recyclerView = view.findViewById(R.id.newUpdatesRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        comicList = new ArrayList<>();
+        adapter = new ComicAdapter(getContext(), comicList);
+        recyclerView.setAdapter(adapter);
+
+        comicList.addAll(ComicDataLoader.loadComics(getContext(), "response_1747039077763.json"));
+        adapter.notifyDataSetChanged();
+
+        return view;
     }
 }
--- app/src/main/java/com/example/apptruyen/firebase/Constants.java
+++ app/src/main/java/com/example/apptruyen/firebase/Constants.java
@@ -1,14 +1,26 @@
 package com.example.apptruyen.firebase;
 
 public class Constants {
+    //user
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_NAME = "name";
     public static final String KEY_EMAIL = "email";
     public static final String KEY_PASSWORD = "password";
+    //Manga
     public static final String MANGA_COLLECTION = "Manga";
     public static final String CHAPTERS_SUBCOLLECTION = "chapters";
+    public static final String KEY_Category = "category";
 
-    // Các hằng số khác (nếu cần)
+    public static final String KEY_THUMB = "thumbUrl";
     public static final String KEY_NAME_MANGA = "name";
     public static final String KEY_STATUS = "status";
+    //
+    public static final String KEY_COLLECTION_CATEGORIES = "categories";
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME_CATEGORY = "name";
+    //
+    public static final String KEY_COLLECTION_CHAPTERS = "chapters";
+    public static final String KEY_CHAPTER_NAME = "chapterName";
+    public static final String KEY_CHAPTER_API_URL = "chapterApiUrl";
+
 }
